695abccf119d639fc8d2ab905a914f741ab2b6d1
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-11-08 23:49:53 +0530

    Added home page

---
+++ frontend/data.php
@@ -0,0 +1,93 @@
+<?php 
+
+    $data = [
+        [
+            "name" => "Rice",
+            "qty" => 0,
+            "price" => 50,
+            "unit" => "kg",
+            "category" => "Daily Necessities",
+            "imageURL" => "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL3Jhd3BpeGVsX29mZmljZV80OV9waG90b19vZl9yaWNlX3NhY2tfd2l0aF9yaWNlX2Nyb3BfaXNvbGF0ZWRfb180MzhlZTY4Zi0wN2IzLTQ2YzctYWE3NC04NzE1N2UwZWUyNzZfMS5qcGc.jpg"
+        ],
+        [
+            "name" => "Wheat Flour",
+            "qty" => 5,
+            "price" => 35,
+            "unit" => "kg",
+            "category" => "Daily Necessities",
+            "imageURL" => "https://png.pngtree.com/background/20230407/original/pngtree-the-scattered-bag-with-wheat-of-a-grain-photo-picture-image_2331187.jpg"
+        ],
+        [
+            "name" => "Toothpaste",
+            "qty" => 5,
+            "price" => 20,
+            "unit" => "N",
+            "category" => "Dental Hygiene",
+            "imageURL" => "https://5.imimg.com/data5/SELLER/Default/2022/7/EC/SZ/AQ/3013777/toothpaste-testing-500x500.jpg"
+        ],
+        [
+            "name" => "Arhar Daal",
+            "qty" => 10,
+            "price" => 90,
+            "unit" => "kg",
+            "category" => "Pulses",
+            "imageURL" => "https://media.istockphoto.com/id/467802772/photo/toor-dal-skinned-and-split-pigeon-pea.jpg?s=612x612&w=0&k=20&c=gZXXcs_zFUtAK8Qnl3F3YNA7gIpxLbTT7hnkmXlfBc4="
+        ],
+        [
+            "name" => "Cumin Seeds",
+            "qty" => 1,
+            "price" => 220,
+            "unit" => "kg",
+            "category" => "Spices",
+            "imageURL" => "https://png.pngtree.com/thumb_back/fh260/background/20230609/pngtree-pile-of-dried-brown-cumin-seeds-on-a-white-background-image_2932243.jpg"
+        ],
+        [
+            "name" => "Red Chilli Powder",
+            "qty" => 0,
+            "price" => 600,
+            "unit" => "kg",
+            "category" => "Spices",
+            "imageURL" => "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAxL3Jhd3BpeGVsb2ZmaWNlMjFfcGhvdG9fb2Zfc3BhbmlzaF9wYXByaWthX2FuZF9wZXBwZXJfcG93ZGVyX2lzb185OTUwN2RlYi1mNjllLTQxOWMtODBjMi02ZjUwZDExMTBjY2ZfMS5qcGc.jpg"
+        ],
+        [
+            "name" => "Moong Daal",
+            "qty" => 19,
+            "price" => 134,
+            "unit" => "kg",
+            "category" => "Pulses",
+            "imageURL" => "https://t3.ftcdn.net/jpg/04/24/47/46/360_F_424474692_iZZEi9t6MU2Lo66rapLWm961YF3TS7x1.jpg"
+        ],
+        [
+            "name" => "Coriander Powder",
+            "qty" => 2.4,
+            "price" => 400,
+            "unit" => "kg",
+            "category" => "Spices",
+            "imageURL" => "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTEwL3Jhd3BpeGVsX29mZmljZV8yMl9ncm91bmRlZF9jZWxlcnlfc2VlZHNfaW5fZ2xhc3NfamFyX2lzb2xhdGVkX184ZTY3YWI4Yi1kZTZiLTRlYTgtYjQxMC01YjI4Yzg3NWJjZmNfMS5qcGc.jpg"
+        ],
+        [
+            "name" => "Turmeric Powder",
+            "qty" => 3,
+            "price" => 320,
+            "unit" => "kg",
+            "category" => "Spices",
+            "imageURL" => "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTEwL3Jhd3BpeGVsb2ZmaWNlNV9waG90b19vZl90dXJtZXJpY19pc29sYXRlZF9vbl90aGVfd2hpdGVfYmFja2dyb185MTdkNjNkNS1hNjY0LTQ0NGQtYWViNS1lMTMxMDRiYmI1M2VfMi5qcGc.jpg"
+        ],
+        [
+            "name" => "Maize Flour",
+            "qty" => 10,
+            "price" => 40,
+            "unit" => "kg",
+            "category" => "Daily Essentials",
+            "imageURL" => "https://5.imimg.com/data5/WX/OA/MY-2/maize-flour-500x500.jpg"
+        ],
+        [
+            "name" => "Yippee",
+            "qty" => 10,
+            "price" => 10,
+            "unit" => "N",
+            "category" => "Snacks",
+            "imageURL" => "https://www.jiomart.com/images/product/original/490818985/sunfeast-yippee-magic-masala-noodles-280-g-product-images-o490818985-p490818985-0-202309022346.jpg?im=Resize=(420,420)"
+        ]
+    ];
+?>
--- frontend/home.php
+++ frontend/home.php
@@ -1,3 +1,5 @@
+<?php require('data.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,6 +9,7 @@
         <link rel="stylesheet" href="default.css">
         <link rel="stylesheet" href="home.module.css">
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <script src="https://kit.fontawesome.com/0a72b2ed27.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <nav>
@@ -22,5 +25,31 @@
                 <button><a href="login.php">Logout</a></button>
             </section>
         </nav>
+        <section class="items">
+            <h1>Items</h1>
+            <section class="items-container">
+                <?php 
+                    $n = count($data);
+                    for($i = 0; $i < $n; $i++)
+                    {
+                        echo '
+                            <section class="card">
+                            <div class="badge">' . $data[$i]["category"] . '</div>
+                            <img src="' . $data[$i]["imageURL"] . '" alt="item-image">
+                            <section class="details">
+                                <h2>' . $data[$i]["name"] . '</h2>
+                                <p>Price: ' . $data[$i]["price"] . '/' . $data[$i]["unit"] . '</p>
+                                <p>Quantity: ' . $data[$i]["qty"] . '</p>
+                                <div class="actions">
+                                    <i class="fa-solid fa-trash" aria-hidden="true"></i>
+                                    <i class="fa-solid fa-pen-to-square" aria-hidden="true"></i>
+                                </div>
+                            </section>
+                        </section>
+                        ';
+                    }
+                ?>
+            </section>
+        </section>
     </body>
 </html>
